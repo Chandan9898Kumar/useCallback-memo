1a19b8c3316d319e1a62d2f483749bd4b0dd7a04
Chandan <Chandan17ee@gmail.com>
2022-11-12 16:36:34 +0530

    fix: fixed test cases

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My React App/);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeDefined()
 });
+
+test('It should be defined', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/My React App/);
+  expect(linkElement).toBeDefined()
+});
+
